d329d7b667ae2be7a1a9b8c9edcc82b5143345dc
rabby9898 <mdfajlerabby00@gmail.com>
2023-12-12 03:28:29 +0600

    contact form added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import AboutMe from "../../Components/AboutMe/AboutMe";
 import Banner from "../../Components/Banner/Banner";
+import Contact from "../../Components/Contact/Contact";
 import Education from "../../Components/Education/Education";
 import Experience from "../../Components/Experience/Experience";
 import Navbar from "../../Components/Navbar/Navbar";
@@ -16,6 +17,7 @@ const Home = () => {
       <Skills></Skills>
       <Experience></Experience>
       <Projects></Projects>
+      <Contact></Contact>
     </div>
   );
 };
